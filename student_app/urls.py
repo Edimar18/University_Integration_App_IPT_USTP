c2368e2748a08727b8e0a0311a060a73a5b136d5
Edimar18 <mosquida.edimar.18@gmail.com>
2026-02-07 18:42:52 +0800

    created a static html file for the 4 tabs

--- student_app/urls.py
+++ student_app/urls.py
@@ -5,5 +5,11 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout')
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('academics/', views.academics, name='academics'),
+    path('library/', views.library, name='library'),
+    path('finance/', views.finance, name='finance'),
+    path('settings/', views.settings, name='settings'),
+
+
 ]
--- student_app/views.py
+++ student_app/views.py
@@ -3,17 +3,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
+import requests
 
 @login_required(login_url='login')
 def home(request):
     student_profile = request.user.studentprofile
+
+    library_app_data = requests.get(f'http://127.0.0.1:8000/api/library/{student_profile.student_id}/').json()
+
     context = {
         'student_id': student_profile.student_id,
         'username': request.user.username,
         'email': request.user.email,
         'course': student_profile.course,
         'year_level': student_profile.year_level,
+        'has_fines': library_app_data['has_fines'],
+        'amount_due': library_app_data['amount_due']
     }
     return render(request, 'student_app/home.html', context)
 
@@ -31,3 +36,20 @@ def login(request):
             return redirect('login')
 
     return render(request, 'student_app/login.html')
+
+@login_required(login_url='login')
+def academics(request):
+    return render(request, 'student_app/academics.html')
+
+
+@login_required(login_url='login')
+def library(request):
+    return render(request, 'student_app/library.html')
+
+@login_required(login_url='login')
+def finance(request):
+    return render(request, 'student_app/finance.html')
+
+@login_required(login_url='login')
+def settings(request):
+    return render(request, 'student_app/settings.html')
